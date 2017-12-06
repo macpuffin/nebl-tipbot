ff32e486f4600d1bfaa4e62e4728f7aee7ef8b4c
NeblioTeam <neblioteam@gmail.com>
2017-12-06 18:48:30 +0000

    Fixed price retrieval due to CMC changes

--- disp.sh
+++ disp.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 a=$(lynx -base -source 'https://coinmarketcap.com/currencies/neblio/' | hxnormalize -x | \
-    hxselect -c -s '\n' "body > div.container > div > div.col-lg-10 > div.row.bottom-margin-1x > div.col-xs-6.col-sm-8.col-md-4.text-left > small.text-gray")
+    hxselect -c -s '\n' "body > div.container > div > div.col-lg-10 > div.row.bottom-margin-1x > div.col-xs-6.col-sm-8.col-md-4.text-left > span.text-gray.details-text-medium")
 str=$(echo $a)
 printf $str
 
