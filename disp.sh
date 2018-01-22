d07666773b7162c7a1f4dd7181ab5d4296d3c804
NeblioTeam <neblioteam@gmail.com>
2018-01-22 15:49:22 +0000

    CMC changed their website and broke tipbot's price feature again

--- disp.sh
+++ disp.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 a=$(lynx -base -source 'https://coinmarketcap.com/currencies/neblio/' | hxnormalize -x | \
-    hxselect -c -s '\n' "body > div.container > div > div.col-lg-10 > div.row.bottom-margin-1x > div.col-xs-6.col-sm-8.col-md-4.text-left > span.text-gray.details-text-medium")
+    hxselect -c -s '\n' "body > div.container > div > div.col-lg-10 > div.row.bottom-margin-1x > div.col-xs-6.col-sm-8.col-md-4.text-left > span.text-gray.details-text-medium > span")
 str=$(echo $a)
 printf $str
 
--- fiat.rb
+++ fiat.rb
@@ -10,7 +10,7 @@ k = usd.gsub(/\s+\<.*\>/, "")
 percent = open( "https://coinmarketcap.com/currencies/neblio/")
 documentx = Nokogiri::HTML(percent)
 
-fix = documentx.xpath("/html/body/div[4]/div/div[1]/div[3]/div[2]/span[2]").inner_html
+fix = documentx.xpath("/html/body/div[4]/div/div[1]/div[3]/div[2]/span[2]/span[1]").inner_html
 b = fix.gsub(/\s+/, "")
 x = " Δ #{b}"
 print k ; print x
