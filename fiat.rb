564eba9628f45924204e68932fc30d0f4fc8d8b7
Jamie D <digweed.jamie@gmail.com>
2018-05-02 18:02:35 +0100

    Hopeful Fix for Tipbot

--- fiat.rb
+++ fiat.rb
@@ -4,15 +4,15 @@ require 'open-uri'
 fiat = open( "https://coinmarketcap.com/currencies/neblio/")
 document = Nokogiri::HTML(fiat)
 
-usd = document.xpath("/html/body/div[4]/div/div[1]/div[3]/div[2]/span[1]/span[1]").inner_html
+usd = document.xpath("//*[@id="quote_price"]/span[1]").inner_html
 k = usd.gsub(/\s+\<.*\>/, "")
 
 percent = open( "https://coinmarketcap.com/currencies/neblio/")
 documentx = Nokogiri::HTML(percent)
 
-fix = documentx.xpath("/html/body/div[4]/div/div[1]/div[3]/div[2]/span[2]/span[1]").inner_html
+fix = documentx.xpath("/html/body/div[6]/div/div[1]/div[4]/div[2]/span[2]").inner_html
 b = fix.gsub(/\s+/, "")
-x = " Δ #{b}"
+x = " #{b}"
 print k ; print x
 
 
