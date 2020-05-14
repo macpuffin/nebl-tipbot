37cd2d4a286ab90bbe74308869f5c2617824e53a
macpuffin <54039421+macpuffin@users.noreply.github.com>
2020-05-14 16:03:48 -0300

    Update and rename neblio.rb to macpuffins.rb

---
+++ coin_config/macpuffins.rb
@@ -0,0 +1,35 @@
+
+#!/usr/bin/ruby
+module Macpuffins
+  PERFORM_ERROR = 'Invalid crypto command.'
+  BALANCE_REPLY_PRETEXT = 'Your current balance is:'
+  CURRENCY_ICON = ' PFN :macpuffins:'
+  WEALTHY_UPPER_BOUND = 10000
+  WEALTHY_UPPER_BOUND_POSTTEXT = ' Rolling in PFN!'
+  WEALTHY_UPPER_BOUND_EMOJI = ':macpuffins:'
+  BALANCE_REPLY_POSTTEXT = ' '
+  DEPOSIT_PRETEXT = 'Enter an amount for deposit ->'
+  DEPOSIT_POSTTEXT = '<- this right here is yer deposit address.'
+  TIP_ERROR_TEXT = 'Please say -> tip @username amount'
+  TIP_PRETEXT = 'Hows she goin, by? Send a few PFN to a Caper what deserves a couplefew!'
+  TIP_POSTTEXT1 = 'http://explorer.macpuffins.com/tx/'
+  TIP_POSTTEXT2 = ' |View transaction on MacPuffins Block Explorer.'
+  WITHDRAW_TEXT = 'Withdraw yer MacPuffins'
+  WITHDRAW_ICON = ':macpuffins:'
+  NETWORKINFO_ICON = ':bar_chart:'
+  TOO_POOR_TEXT = 'Gway wit ya, by, you gotta have at least 0.0001 PFN left over in yer wallet after you send a tip. '
+  PRICE_PRE = 'The current price of PFN :macpuffins: is: '
+  HELP = 'Commands are used by saying tipbot and one of the following: deposit, balance, tip, withdraw, leaderboard,  price, chart or about. Try my friend `Trifbot` if you want to tip in Macpuffin NTP1 tokens.'
+  NO_PURPOSE_LOWER_BOUND_TEXT = 'You call THAT a tip, ya chintzy bastard?'
+  NO_PURPOSE_LOWER_BOUND = 0.000001
+  HI = 'Céad míle fáilte!'
+  GREETING = 'Hows she goin, by? Welcome to the MacPuffin TipBot'
+  RANDOMIZED_EMOJI = ':macpuffins:'
+  NETWORK = 'macpuffins'
+  ABOUT = 'Compiled by @macpuffins, view source code on '
+  ABOUT2 = 'https://github.com/macpuffin/pfn
+-tipbot'
+  FEE = 'Attempt compensating for tx fee.'
+  SYM = 'BTC :bitcoin:'
+  BTC = 'Which is approximately equal to '
+end
