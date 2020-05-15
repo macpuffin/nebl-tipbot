738df28a3c153e69a06b39f0300958af6d7c5032
macpuffin <54039421+macpuffin@users.noreply.github.com>
2020-05-15 15:31:09 -0300

    Update bitcoin_client_extensions.rb

--- bitcoin_client_extensions.rb
+++ bitcoin_client_extensions.rb
@@ -2,7 +2,7 @@ module Bitcoin
   class Client
     def self.local
       return Bitcoin::Client.new(ENV['RPC_USER'], ENV['RPC_PASSWORD'],
-        { host: '127.0.0.1', port: 6326, ssl: false} )
+        { host: '127.0.0.1', port: 4020, ssl: false} )
     end
   end
 end
